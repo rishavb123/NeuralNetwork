51b5536de43b06eb62e9a340e5bfa522035d5055
Rishav Bhagat <rishavb123@gmail.com>
2019-02-03 16:22:05 -0500

    making chrome console playground to test the java

---
+++ playground.js
@@ -0,0 +1,20 @@
+const java = require('java');
+
+java.classpath.push('bin');
+java.classpath.push('target/classes');
+
+// create a server that sends these objects to the client for use
+const NeuralNetwork = java.import('io.bhagat.artificialintelligence.NeuralNetwork');
+const Perceptron = java.import('io.bhagat.artificialintelligence.Perceptron');
+const Matrix = java.import('io.bhagat.math.Matrix');
+
+// let m1 = new Matrix(2, 3);
+// console.log(m1.toString());
+// let vectors = m1.getVectorColumnsSync();
+// for (let v of vectors)
+//     console.log(v.toString());
+
+// let p = new Perceptron(2, 10, 0.99, 100);
+
+let nn = new NeuralNetwork(java.newArray("int", [3, 8, 102, 2, 8, 1]));
+console.log(nn.feedForwardSync(java.newArray("double", [2, 2, 2])));
