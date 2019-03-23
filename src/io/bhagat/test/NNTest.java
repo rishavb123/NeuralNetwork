08acb55b6bb4116da322619a8d5d55dfe78060a9
Rishav Bhagat <rishavb123@gmail.com>
2019-03-23 13:15:17 -0400

    switched the comments

--- src/io/bhagat/test/NNTest.java
+++ src/io/bhagat/test/NNTest.java
@@ -9,47 +9,47 @@ import io.bhagat.util.SerializableUtil;
 public class NNTest {
 
 	public static void main(String[] args) {
-		NeuralNetwork nn = new NeuralNetwork(2, 4, 1);
-		
-		double[][] inputs = new double[][] { 
-			{1, 0},
-			{0, 1},
-			{1, 1},
-			{0, 0}
-		};
-		double[][] targets = new double[][] { 
-			{1},
-			{1},
-			{0},
-			{0}
-		};
-		
-		for(int i = 0; i < 10000000; i++)
-		{
-			int index = (int)(Math.random()*4);
-			nn.train(inputs[index], targets[index]);
-		}
-		
-		try {
-			SerializableUtil.serialize(nn, "xor_neural_network.ser");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		ArrayUtil.printArr(nn.feedForward(1, 0));
-		ArrayUtil.printArr(nn.feedForward(0, 1));
-		ArrayUtil.printArr(nn.feedForward(1, 1));
-		ArrayUtil.printArr(nn.feedForward(0, 0));
-		
+//		NeuralNetwork nn = new NeuralNetwork(2, 4, 1);
+//		
+//		double[][] inputs = new double[][] { 
+//			{1, 0},
+//			{0, 1},
+//			{1, 1},
+//			{0, 0}
+//		};
+//		double[][] targets = new double[][] { 
+//			{1},
+//			{1},
+//			{0},
+//			{0}
+//		};
+//		
+//		for(int i = 0; i < 10000000; i++)
+//		{
+//			int index = (int)(Math.random()*4);
+//			nn.train(inputs[index], targets[index]);
+//		}
+//		
 //		try {
-//			NeuralNetwork nn = SerializableUtil.deserialize("xor_neural_network.ser");
-//			ArrayUtil.printArr(nn.feedForward(1, 0));
-//			ArrayUtil.printArr(nn.feedForward(0, 1));
-//			ArrayUtil.printArr(nn.feedForward(1, 1));
-//			ArrayUtil.printArr(nn.feedForward(0, 0));
-//		} catch (ClassNotFoundException | IOException e) {
+//			SerializableUtil.serialize(nn, "xor_neural_network.ser");
+//		} catch (IOException e) {
 //			e.printStackTrace();
 //		}
+//
+//		ArrayUtil.printArr(nn.feedForward(1, 0));
+//		ArrayUtil.printArr(nn.feedForward(0, 1));
+//		ArrayUtil.printArr(nn.feedForward(1, 1));
+//		ArrayUtil.printArr(nn.feedForward(0, 0));
+		
+		try {
+			NeuralNetwork nn = SerializableUtil.deserialize("xor_neural_network.ser");
+			ArrayUtil.printArr(nn.feedForward(1, 0));
+			ArrayUtil.printArr(nn.feedForward(0, 1));
+			ArrayUtil.printArr(nn.feedForward(1, 1));
+			ArrayUtil.printArr(nn.feedForward(0, 0));
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
 		
 		
 		
