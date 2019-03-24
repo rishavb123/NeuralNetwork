95171c7d4a5f0490cf82da5f86657fb1becf4a2c
Rishav Bhagat <rishavb123@gmail.com>
2019-03-24 12:38:16 -0400

    created new docs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.bhagat.artificialintelligence"},{"l":"io.bhagat.math"},{"l":"io.bhagat.math.calculus"},{"l":"io.bhagat.math.linearalgebra"},{"l":"io.bhagat.math.statistics"},{"l":"io.bhagat.server"},{"l":"io.bhagat.util"}]
