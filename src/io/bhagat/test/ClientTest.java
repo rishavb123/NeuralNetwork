3de5f6c4b60b58d25b88f168e4ca3d4078b6de58
Rishav Bhagat <rishavb123@gmail.com>
2019-03-29 17:01:29 -0400

    changed ip

--- src/io/bhagat/test/ClientTest.java
+++ src/io/bhagat/test/ClientTest.java
@@ -9,7 +9,7 @@ public class ClientTest {
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		Client client = new Client("192.168.1.33", 8000);
+		Client client = new Client("172.20.10.5", 8000);
 		client.start();
 		String msg;
 		while(!(msg = scanner.nextLine()).equals("exit") && client.isAlive())
