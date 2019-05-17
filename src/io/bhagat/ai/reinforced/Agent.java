3ce034f762d89869e7b986592cea3ca4608292dc
Rishav Bhagat <rishavb123@gmail.com>
2019-05-16 23:23:07 -0400

    created basic classes

---
+++ src/io/bhagat/ai/reinforced/Agent.java
@@ -0,0 +1,11 @@
+package io.bhagat.ai.reinforced;
+
+/**
+ * a class for a reinforcement learning agent
+ * @author Bhagat
+ */
+public abstract class Agent {
+
+	
+
+}
---
+++ src/io/bhagat/ai/reinforced/Environment.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package io.bhagat.ai.reinforced;
+
+/**
+ * a class for an environment for the agent to learn in 
+ * @author Bhagat
+ */
+public abstract class Environment {
+
+}
